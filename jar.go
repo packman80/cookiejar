df380c66e0e9dba1eed4ab12982c8419c1d5505d
Shine <shine@selytics-team.ru>
2023-10-16 04:39:43 +0300

    add external structure

--- jar.go
+++ jar.go
@@ -110,6 +110,26 @@ type entry struct {
 	seqNum uint64
 }
 
+type Entry struct {
+	Name       string
+	Value      string
+	Domain     string
+	Path       string
+	SameSite   string
+	Secure     bool
+	HttpOnly   bool
+	Persistent bool
+	HostOnly   bool
+	Expires    time.Time
+	Creation   time.Time
+	LastAccess time.Time
+
+	// seqNum is a sequence number so that Cookies returns cookies in a
+	// deterministic order, even for cookies that have equal Path length and
+	// equal Creation time. This simplifies testing.
+	seqNum uint64
+}
+
 // id returns the domain;path;name triple of e as an id.
 func (e *entry) id() string {
 	return fmt.Sprintf("%s;%s;%s", e.Domain, e.Path, e.Name)
